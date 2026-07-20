1cdbbb8ace6fb3986860d354c448c912914bbf09
oak <oak07951@gmail.com>
2026-07-20 14:45:14 +0700

    Update class-componnent.js

--- src/class-componnent.js
+++ src/class-componnent.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default class Calendar extends React.Component {
-    getData() {
+    getDate() {
         const dayNames = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์'];
         const monthNames = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
         const date = new Date();
+        const weekDay = dayNames[date.getDay()]
         const day = date.getDate();
-        const month = date.getMonth();
+        const month = monthNames[date.getMonth()]
         const year = date.getFullYear();
-
-        return { day, month, year, dayNames, monthNames };
+        return `วัน${weekDay}ที่ ${day} ${month} ${year}`
     }
     render() {
-    return <div>{this.getData().day}</div>;
+    return <div>{this.getDate()}</div>;
     }
 }
