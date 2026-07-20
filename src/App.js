4121084359322ad819e1e0bf9bce1be02d678c3c
oak <oak07951@gmail.com>
2026-07-20 14:05:37 +0700

    function componnet
    header center footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import './App.css';
-import './style.css'
- let r = 'React'
-  let rn = 'React Native'
-  const el =(
-    <div>
-      Create Web & Moblie Apps<br/>
-      with {r}&{rn}
-    </div>
-  )
+
+import { Content, Footer, Header } from './func-componnet';
+
+
 
 function App() {
   
   return ( <>
-  <h2>DeveloperThai.com</h2>
-  <div className='title'>React & React Native</div>
+  <Header />
+  <Content />
+  <Footer />
   </>
     );
 }
