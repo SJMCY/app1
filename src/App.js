06622a5f370cfb7756cedba61f76a5430ade1512
oak <oak07951@gmail.com>
2026-08-10 11:17:05 +0700

    state-func
    state-func

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import RefsClass from './refs-func';
-import { EventData2 } from './event-data';
-import RefsArray from './refs-array';
+import StateFunc from './state-func';
+
 
 function App() {
 
   return (
   
-    <RefsArray/>
+    <StateFunc/>
   )
 }
 
