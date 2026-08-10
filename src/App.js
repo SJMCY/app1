cf95590ef5ef936f3ce54f8f3a6c9c4730f7b5c6
oak <oak07951@gmail.com>
2026-08-10 09:56:49 +0700

    refs-func
    refs-func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import RefsClass from './refs-func';
 import { EventData2 } from './event-data';
 
 function App() {
 
   return (
   
-    <EventData2/>
+    <RefsClass/>
   )
 }
 
