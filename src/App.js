e16b36a54f4014b4ef32226531ff53f75398a0af
oak <oak07951@gmail.com>
2026-08-10 10:54:23 +0700

    refs-array
    refs-array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import RefsClass from './refs-func';
 import { EventData2 } from './event-data';
+import RefsArray from './refs-array';
 
 function App() {
 
   return (
   
-    <RefsClass/>
+    <RefsArray/>
   )
 }
 
